768824c62d12147e71586cd83f4c2bdda47ccbbd
consuman <nicolai@consuman.de>
2020-03-22 13:25:26 +0100

    ssl connection for backend

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 import pkg from '~/package'
 
-const socket = io.connect(`ws://${pkg.backend}/`)
+const socket = io.connect(`wss://${pkg.backend}/`)
 
 export default socket
